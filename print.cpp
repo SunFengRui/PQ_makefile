0ac475df7f4ca800b7d355c19b2f03d2e3f3e694
SunFengRui <38269977+SunFengRui@users.noreply.github.com>
2020-06-03 13:10:08 +0800

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -4,9 +4,14 @@
 
 void *printFunc(void *arg)
 {
+    static int print_count=0;
     while(1)
     {
+        printf("     %d     \n",print_count);
         printf("fre: %.5f\n",A_fre);
+        printf("loss: %d\n",A_fre);
+        printf("                \n")
+        print_count++;
         sleep(1);
 
     }
